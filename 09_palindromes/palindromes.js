1c7a1120750cbb968c1dee156c7a682d9a12d3e5
Paras Sethi <113545803+ParasSethi737@users.noreply.github.com>
2024-01-17 03:45:11 +0530

    Completed 12_findTheOldest

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const modifiedString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return modifiedString === modifiedString.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let count
+    if (typeof n !== 'number') {
+        count = parseInt(n)
+    } else {
+        count = n
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let first=1;
+    let second=1
+    for(let i=2;i<n;i++){
+        let current = first+second
+        first=second
+        second=current
+    }
+    return second;
 };
 
 // Do not edit below this line
